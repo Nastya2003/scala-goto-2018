a744d6159bedaab833a15618448a85a8e48129dd
Nastya2003 <33423863+Nastya2003@users.noreply.github.com>
2018-08-18 20:00:37 +0300

    Update task1.scala

--- task1.scala
+++ task1.scala
@@ -6,6 +6,7 @@
 
   object day3Base {
     def isOdd(x: Int): Boolean = x % 2 == 0
+
     def isEven(x: Int): Boolean = x % 2 != 0
 
     def sumOfDigits(x: Int): Int =
@@ -13,4 +14,28 @@
 
     def compositionOfDigits(x: Int): Int =
       (x / 10) * sumOfDigits(x - x / 10)
+
+    def nthGreatestDivisor(x: Int, b: List[Int] = List(), a: List[Int]): List[Int] = a match {
+      case (elem :: Nil) if (x % elem == 0) => b :+ elem
+      case (elem :: tail) if (x % elem == 0) => nthGreatestDivisor(tail.toString.toInt, b :+ elem, a)
+    }
+
+    def nthGreatestDivisor2(b: List[Int], n: Int): Int = b match {
+      case (Nil) => 0
+      case (elem :: tail) => nthGreatestDivisor2(tail, n-1)
+    }
+
+    def numOfDivisors(b: List[Int]): Int = b.length
+
+    def sumOfDivisors(b: List[Int]): Int = b match {
+      case (Nil) => 0
+      case (elem :: tail) => elem + sumOfDivisors(tail)
+    }
+
+    def isPrime(a: Int): Boolean = {
+      var b: List[Int] = List()
+      var c: List[Int] = List()
+      b = nthGreatestDivisor(a, b, c)
+      if (b.length == 2) true else false
+    }
   }
