c786d3902f38dab51ebdfe29b36cf792065d359d
Nastya2003 <33423863+Nastya2003@users.noreply.github.com>
2018-08-18 10:17:05 +0300

    Create task1.scala

---
+++ task1.scala
@@ -0,0 +1,13 @@
+ object day2Hard {
+    def duplicate(n: Int, a: List[Char]): List[Char] = {
+      a.map(i => List.fill(n)(i)).flatten
+    }
+  }
+
+  object day3Base {
+    def isOdd(x: Int): Boolean = x % 2 == 0
+    def isEven(x: Int): Boolean = x % 2 != 0
+
+    def sumOfDigits(x: Int): Int = 
+      x = x / 10 + sumOfDigits(x - x / 10)
+  }
