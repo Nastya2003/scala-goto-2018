75de4007b4b1427ebab878adafbce024e5a9f39c
Nastya2003 <33423863+Nastya2003@users.noreply.github.com>
2018-08-18 19:14:42 +0300

    Update actor1.scala

--- actor1.scala
+++ actor1.scala
@@ -1,8 +1,6 @@
-
 import akka.actor._
 import akka.pattern.{ask, pipe}
 import akka.util.Timeout
-
 import scala.collection._
 import scala.concurrent.duration._
 import scala.concurrent.ExecutionContext.Implicits.global
@@ -15,18 +13,17 @@ class aucActor extends Actor with ActorLogging {
   val state: mutable.Map[String, Int] = mutable.Map.empty
   var currPrice = 0
   def receive = {
-    case r: set =>
-      state += r.a -> r.b
-    case r: bet if state(r.a) <= r.b =>
-      log.warning(s"Bought by ${r.b}")
+    case r: set => state += r.a -> r.b
+    case r : bet if (state.get(r.a) == None) => log.warning(s"${r.a} was already bought")
+    case r: bet if state(r.a) <= r.b => {
+      log.warning(s"${r.a} bought by ${r.b}")
+      state -= r.a
+    }
     case r: bet if currPrice < r.b => {
       currPrice = r.b
-      log.warning(s"Current prise is: ${currPrice}")
+      log.warning(s"${r.a} current prise is: ${currPrice}")
     }
-    case r: bet =>
-      log.warning(s"Minimum price is: ${r.b}")
-    case r =>
-      log.warning(s"Unexpected: $r")
+    case r: bet => log.warning(s"${r.a} minimum price is: ${r.b}")
   }
 }
 
@@ -43,16 +40,31 @@ class mainActor extends Actor with ActorLogging {
   def receive = {
     case Start =>
       aucActor ! set("gold", 150)
-      log.warning(s"First prise: 150")
+      log.warning(s"Gold first prise: 150")
+      aucActor ! set("silver", 100)
+      log.warning(s"Silver first prise: 100")
+      aucActor ! set("brilliant", 400)
+      log.warning(s"Brilliant first prise: 400")
 
       aucActor ! bet("gold", 120)
       aucActor ! bet("gold", 80)
+      aucActor ! bet("silver", 90)
       aucActor ! bet("gold", 200)
+      aucActor ! bet("brilliant", 300)
+      aucActor ! bet("silver", 70)
+      aucActor ! bet("brilliant", 250)
+      aucActor ! bet("silver", 120)
+      aucActor ! bet("brilliant", 400)
       context.system.terminate()
   }
 }
 
-
 object Main extends App {
   val system = ActorSystem("system")
   val mainActor = system.actorOf(Props(new mainActor), "mainActor")
+}
+
+
+
+
+
