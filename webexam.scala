584645a369be24ca1f26e572836a6e179d369dc7
Nastya2003 <33423863+Nastya2003@users.noreply.github.com>
2018-08-19 16:20:45 +0300

    Update webexam.scala

--- webexam.scala
+++ webexam.scala
@@ -1,1 +1,4 @@
-
+def task1(a: List[Future[Try[Option[Int]]]], b: List[Future[Try[Option[Int]]]] = List()): List[Future[Try[Option[Int]]]] = a match {
+  case (Nil) => Nil
+  case (elem :: tail) => task1(tail, tail :+ elem.future.map(_.map(_.map(elem => elem * 10))))
+}
