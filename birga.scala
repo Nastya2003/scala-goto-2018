2a63c8dfb8aab38e1504d407dfc0d675d4206b42
Nastya2003 <33423863+Nastya2003@users.noreply.github.com>
2018-08-23 13:04:55 +0300

    Update birga.scala

--- birga.scala
+++ birga.scala
@@ -1,124 +1,150 @@
-import akka.actor._
-import akka.pattern.{ask, pipe}
-import akka.util.Timeout
-
-import scala.collection._
 import scala.collection.mutable.ListBuffer
-import scala.concurrent.duration._
-import scala.concurrent.ExecutionContext.Implicits.global
-import scala.io.Source
-
-//case class c1(ds: Int, a: Int, b: Int, c: Int, d: Int)
-//case class c2(ds: Int, a: Int, b: Int, c: Int, d: Int)
-//case class c3(ds: Int, a: Int, b: Int, c: Int, d: Int)
-//case class act(cl: Any, ac: String, vl: Char, pr: Int, n: Int)
-//
-//
-//class buy extends Actor with ActorLogging {
-//
-//}
-//
-//class sell extends Actor with ActorLogging {
-//
-//}
 
 object Main extends App {
-//  val system = ActorSystem("system")
-//  val buy = system.actorOf(Props(new buy), "buy")
-//  val sell = system.actorOf(Props(new sell), "sell")
+  import acts._
   var balance: ListBuffer[(String, Int, Int, Int, Int, Int)] =
     ListBuffer(("c1", 1000, 10, 5, 15, 0), ("c2", 200,  5, 10, 0, 15), ("c3", 500, 15, 5, 10, 0))
   val actions: ListBuffer[(String, String, String, Int, Int)] =
     ListBuffer(("c1", "buy", "A", 7, 12), ("c2", "sell", "D", 10, 10), ("c3", "buy", "D", 10, 10), ("c3", "sell", "A", 7, 12))
+  action(actions)
+  println(balance)
+  println(actions)
 }
 
 object acts {
+
   import Main._
-  def action(a: ListBuffer[(String, String, String, Int, Int)]): Unit = a match {
-    case elem if (elem.head._2 == "buy" || elem.head._2 == "sell") => {
-      search(elem.head, elem.tail)
-      action(elem.tail)
-     }
-  }
 
-
-  def search(e: (String, String, String, Int, Int), t: ListBuffer[(String, String, String, Int, Int)]): Unit = t match {
-    case elem => {
-      if (((elem.head._1 != e._1) && (elem.head._2 != e._2) && (elem.head._3 == e._3)) &&
-        ((elem.head._4 == e._4) && (elem.head._5 != e._5))) {
-        make(e, elem.head)
-        search(e, elem.tail)
-      } else _
+  def action(a: ListBuffer[(String, String, String, Int, Int)]): Boolean = {
+    if (a nonEmpty) {
+      if (a.head._2 == "buy" || a.head._2 == "sell") {
+        search(a.head, a.tail)
+        action(a.tail)
+        true
+      }
+      else false
     }
+    else false
   }
 
 
-  def make(a: (String, String, String, Int, Int), b: (String, String, String, Int, Int)): Unit = {
+  def search(e: (String, String, String, Int, Int), t: ListBuffer[(String, String, String, Int, Int)]): Boolean = {
+    if (t nonEmpty) {
+      if (((t.head._1 != e._1) && (t.head._2 != e._2) && (t.head._3 == e._3)) &&
+        ((t.head._4 == e._4) && (t.head._5 == e._5))) {
+        make(e, t.head)
+        search(e, t.tail)
+        true
+      }
+      else false
+    }
+    else false
+  }
+
+
+  def make(a: (String, String, String, Int, Int), b: (String, String, String, Int, Int)): Boolean = {
     if (a._2 == "sell") {
-      findCheckSell(a, balance)
-      findCheckBuy(b, balance)
+      if (checkSell(a, b, balance) && checkBuy(a, b, balance)) {
+        doSell(a, balance)
+        doBuy(b, balance)
+        delete(a, b, actions)
+        true
+      }
+      else false
     }
     else if (b._2 == "sell") {
-      findCheckSell(a, balance)
-      findCheckBuy(b, balance)
+      if (checkSell(b, a, balance) && checkBuy(b, a, balance)) {
+        doSell(b, balance)
+        doBuy(a, balance)
+        delete(a, b, actions)
+        true
+      }
+      else false
     }
-    else _
+    else false
   }
 
 
-  def findCheckSell(sell: (String, String, String, Int, Int), bal: ListBuffer[(String, Int, Int, Int, Int, Int)]): Unit = {
-    if ((sell._3 == "A") && (balance(findBal(sell._1, bal))._3 > sell._5)) {
-      val x = balance(findBal(sell._1, bal))._3
-      val old = balance(findBal(sell._1, bal))
-      val new2 = (old._1, old._2, old._3 - sell._5, old._4, old._5, old._6)
-      balance(findBal(sell._1, bal)) = new2
+  def checkSell(sell: (String, String, String, Int, Int), buy: (String, String, String, Int, Int),
+                bal: ListBuffer[(String, Int, Int, Int, Int, Int)]): Boolean = {
+    if ((sell._3 == "A") && (balance(findBal(sell._1, bal))._3 > sell._5)) true
+    else if ((sell._3 == "B") && (balance(findBal(sell._1, bal))._4 > sell._5)) true
+    else if ((sell._3 == "C") && (balance(findBal(sell._1, bal))._5 > sell._5)) true
+    else if ((sell._3 == "D") && (balance(findBal(sell._1, bal))._6 > sell._5)) true
+    else false
+  }
 
-      val old2 = actions(findAct(sell, actions))
-      val new3 = (old2._1, old2._2, old2._3, old2._4, old2._5 + x)
-      actions(findAct(sell, actions))   = new3
+
+  def checkBuy(sell: (String, String, String, Int, Int), buy: (String, String, String, Int, Int),
+               bal: ListBuffer[(String, Int, Int, Int, Int, Int)]): Boolean = {
+    if (balance(findBal(buy._1, balance))._2 > actions(findAct(buy, actions))._4 * actions(findAct(buy, actions))._5) true
+    else false
+  }
+
+
+  def doSell(sell: (String, String, String, Int, Int), bal: ListBuffer[(String, Int, Int, Int, Int, Int)]): Boolean = {
+    if ((sell._3 == "A") && (balance(findBal(sell._1, bal))._3 > sell._5)) {
+      val old = balance(findBal(sell._1, bal))
+      val new2 = (old._1, old._2 + sell._4 * sell._5, old._3 - sell._5, old._4, old._5, old._6)
+      balance(findBal(sell._1, bal)) = new2
+      true
     }
 
-    else if ((sell._3 == "B") && (balance(findBal(sell._1, bal))._4 > sell._5)) {
-      val x = balance(findBal(sell._1, bal))._4
+    else if ((sell._3 == "B") && (balance(findBal(sell._1, bal))._4 > sell._5)){
       val old = balance(findBal(sell._1, bal))
-      val new2 = (old._1, old._2, old._3, old._4 - sell._5, old._5, old._6)
+      val new2 = (old._1, old._2 + sell._4 * sell._5, old._3, old._4 - sell._5, old._5, old._6)
       balance(findBal(sell._1, bal)) = new2
-
-      val old2 = actions(findAct(sell, actions))
-      val new3 = (old2._1, old2._2, old2._3, old2._4, old2._5 + x)
-      actions(findAct(sell, actions))   = new3
+      true
     }
 
     else if ((sell._3 == "C") && (balance(findBal(sell._1, bal))._5 > sell._5)) {
-      val x = balance(findBal(sell._1, bal))._5
       val old = balance(findBal(sell._1, bal))
-      val new2 = (old._1, old._2, old._3, old._4, old._5 - sell._5, old._6)
+      val new2 = (old._1, old._2 + sell._4 * sell._5, old._3, old._4, old._5 - sell._5, old._6)
       balance(findBal(sell._1, bal)) = new2
-
-      val old2 = actions(findAct(sell, actions))
-      val new3 = (old2._1, old2._2, old2._3, old2._4, old2._5 + x)
-      actions(findAct(sell, actions))   = new3
+      true
     }
 
     else if ((sell._3 == "D") && (balance(findBal(sell._1, bal))._6 > sell._5)) {
-      val x = balance(findBal(sell._1, bal))._6
       val old = balance(findBal(sell._1, bal))
-      val new2 = (old._1, old._2, old._3, old._4, old._5, old._6 - sell._5)
+      val new2 = (old._1, old._2 + sell._4 * sell._5, old._3, old._4, old._5, old._6 - sell._5)
       balance(findBal(sell._1, bal)) = new2
-
-      val old2 = actions(findAct(sell, actions))
-      val new3 = (old2._1, old2._2, old2._3, old2._4, old2._5 + x)
-      actions(findAct(sell, actions))   = new3
+      true
     }
-    else _
+    else false
    }
 
 
-  def findCheckBuy(buy: (String, String, String, Int, Int), bal: ListBuffer[(String, Int, Int, Int, Int, Int)]): Unit = {
-    if (balance(findBal(buy._1, balance))._2 > actions(findAct(buy, actions))._4 * actions(findAct(buy, actions))._5) {
-
+  def doBuy(buy: (String, String, String, Int, Int), bal: ListBuffer[(String, Int, Int, Int, Int, Int)]): Boolean = {
+    val money = actions(findAct(buy, actions))._4 * actions(findAct(buy, actions))._5
+    if (buy._3 == "A" && balance(findBal(buy._1, balance))._2 > money) {
+      val old = balance(findBal(buy._1, balance))
+      val new2 = (old._1, old._2 - money, old._3 + buy._5, old._4, old._5, old._6)
+      balance(findBal(buy._1, balance)) = new2
+      true
     }
-    
+
+    else if (buy._3 == "B" && balance(findBal(buy._1, balance))._2 > money) {
+      val old = balance(findBal(buy._1, balance))
+      val new2 = (old._1, old._2 - money, old._3, old._4 + buy._5, old._5, old._6)
+      balance(findBal(buy._1, balance)) = new2
+      true
+    }
+
+    else if (buy._3 == "C" && balance(findBal(buy._1, balance))._2 > money) {
+      val old = balance(findBal(buy._1, balance))
+      val new2 = (old._1, old._2 - money, old._3, old._4, old._5 + buy._5, old._6)
+      balance(findBal(buy._1, balance)) = new2
+      true
+    }
+
+    else if (buy._3 == "D" && balance(findBal(buy._1, balance))._2 > money) {
+      val old = balance(findBal(buy._1, balance))
+      val new2 = (old._1, old._2 - money, old._3, old._4 , old._5, old._6 + buy._5)
+      balance(findBal(buy._1, balance)) = new2
+      true
+    }
+
+    else false
   }
 
 
@@ -129,11 +155,18 @@ object acts {
     }
   }
 
-  def findAct(a: (String, String, String, Int, Int), act: ListBuffer[(String, String, String, Int, Int)], k: Int = 0): Int = act match {
-    case elem => {
-        if (a == elem) k
-        else findAct(a, elem.tail, k + 1)
+
+  def findAct(a: (String, String, String, Int, Int), act: ListBuffer[(String, String, String, Int, Int)], k: Int = 0): Int = {
+    if (act nonEmpty) {
+        if (a == act.head) k
+        else findAct(a, act.tail, k + 1)
     }
+    else k
+  }
+
+  def delete(sell: (String, String, String, Int, Int), buy: (String, String, String, Int, Int),
+             act: ListBuffer[(String, String, String, Int, Int)]): Unit = {
+    actions.remove(findAct(sell, act))
+    actions.remove(findAct(buy, act))
   }
 }
-
